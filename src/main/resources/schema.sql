7413502685480df56892df25870418ed196bb59d
m4rv1n33 <marvin.m.strasser@gmail.com>
2025-01-09 18:32:00 +0100

    Minor changes to schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,8 +5,8 @@ USE banksystem;
 
 -- Create the Account table
 CREATE TABLE Account (
-                         id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                         balance DECIMAL(10,2) NOT NULL,
+                         id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                         balance DECIMAL(10,2) NOT NULL DEFAULT '0',
                          name VARCHAR(100) NOT NULL,
                          pin INT NOT NULL
 );
@@ -17,14 +17,14 @@ CREATE TABLE Transaction (
                              account_id INT,
                              type VARCHAR(50),
                              amount DECIMAL(10,2),
-                             timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                             timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
                              FOREIGN KEY (account_id) REFERENCES Account(id)
 );
 
 -- Insert sample data into the Account table
-INSERT INTO Account (id, balance, name, pin) VALUES
-                                                 (1, 100.00, 'Marvin Strasser', 1111),
-                                                 (2, 200.00, 'John Doe', 2222),
-                                                 (3, 300.00, 'Jane Doe', 3333),
-                                                 (4, 400.00, 'John Smith', 4444),
-                                                 (5, 500.00, 'Jane Smith', 5555);
+INSERT INTO Account (name, pin) VALUES
+                                                 ('Marvin Strasser', 1111),
+                                                 ('John Doe', 2222),
+                                                 ('Jane Doe', 3333),
+                                                 ('John Smith', 4444),
+                                                 ('Jane Smith', 5555);
