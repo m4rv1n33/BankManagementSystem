b62b1d550247c098fdabf5a43eec05035e3babf3
m4rv1n33 <marvin.m.strasser@gmail.com>
2025-01-10 12:59:57 +0100

    WOP Back to Menu button in transfer.html

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,27 +4,29 @@ CREATE DATABASE IF NOT EXISTS banksystem;
 USE banksystem;
 
 -- Create the Account table
-CREATE TABLE Account (
-                         id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-                         balance DECIMAL(10,2) NOT NULL DEFAULT '0',
-                         name VARCHAR(100) NOT NULL,
-                         pin INT NOT NULL
+CREATE TABLE Account
+(
+    id      INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    balance DECIMAL(10, 2)                 NOT NULL DEFAULT '0',
+    name    VARCHAR(100)                   NOT NULL,
+    pin     INT                            NOT NULL
 );
 
 -- Create the Transaction table
-CREATE TABLE Transaction (
-                             id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-                             account_id INT,
-                             type VARCHAR(50),
-                             amount DECIMAL(10,2),
-                             timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-                             FOREIGN KEY (account_id) REFERENCES Account(id)
+CREATE TABLE Transaction
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY      NOT NULL,
+    account_id INT,
+    type       VARCHAR(50),
+    amount     DECIMAL(10, 2),
+    timestamp  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES Account (id)
 );
 
 -- Insert sample data into the Account table
-INSERT INTO Account (name, pin) VALUES
-                                                 ('Marvin Strasser', 1111),
-                                                 ('John Doe', 2222),
-                                                 ('Jane Doe', 3333),
-                                                 ('John Smith', 4444),
-                                                 ('Jane Smith', 5555);
+INSERT INTO Account (name, pin)
+VALUES ('Marvin Strasser', 1111),
+       ('John Doe', 2222),
+       ('Jane Doe', 3333),
+       ('John Smith', 4444),
+       ('Jane Smith', 5555);
